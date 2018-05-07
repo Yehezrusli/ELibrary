532790b684c5e047f54e0742ecddfd69280809ef
jy006 <yehezkieljason@gmail.com>
2018-05-07 16:57:52 +0700

    edit usr.php

--- pages/user/usr.php
+++ pages/user/usr.php
@@ -3,17 +3,208 @@
 ?>
 <!DOCTYPE html>
 <html>
-	<head>
+<head>
 		<title>eLibrary</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- OPTIONAL -->
 		<link rel="stylesheet" href="../../lib/w3.css">
+		<link rel="stylesheet" href="../../lib/font-awesome.min.css">
 		<link rel="stylesheet" href="../../lib/w3-theme-riverside.css">
 		<link rel="stylesheet" href="../../style/style.css">
-		<link rel="stylesheet" href="../../lib/font-awesome.min.css">
-		<link rel="stylesheet" href="../../lib/font-awesome.css">
+		<style>
+			input{
+				float: left;
+				margin: 40px;
+				width: 400px;
+			}
+			.w3-theme-l2{
+				width: 50%;
+				margin: auto;
+			}
+
+			#image-box{
+				margin-top: 10px;
+				width: 99%;
+				margin-right: auto;
+				margin-left: auto;
+				box-shadow: 10px 10px 5px black;
+			}
+
+			html, body{
+				background-color: rgb(53, 50, 50);
+				height: 100%;
+			}
+
+			#signup{
+				margin-top: 20px;
+				margin-bottom: 20px;
+				background-color:  rgb(204, 198, 198);
+				width: 99%;
+				box-shadow: 10px 10px 5px black;
+				height: 40px;
+				margin-left: auto;
+				margin-right: auto;
+			}
+
+			input[type=text] {
+				margin: 10px;
+				margin-top: 10px;
+				margin-bottom: 10px;
+				width: 97%;
+				height: 40px;
+				padding-left: 10px;
+				font-size: 20px;
+			}
+
+			input[type=password] {
+				margin: 10px;
+				margin-top: 10px;
+				margin-bottom: 10px;
+				width: 97%;
+				height: 40px;
+				padding-left: 10px;
+				font-size: 20px;
+			}
+
+			h1{
+				margin-left: 10px;
+			}
+
+			#button{
+				background-color : black;
+				color: white;
+				margin-left: 10px;
+				font-size: 18px;
+				padding: 5px;
+				padding-left: 10px;
+				padding-right: 10px;	
+			}
+
+			input[type=submit] {
+            width: 100px;
+			height: 40px;
+			margin: 0;
+			}
+			
+			p{
+				font-size: 20px;
+				margin-left: 10px;
+				display: inline;
+			}
+
+			.modal {
+            position: fixed;
+            z-index: 1;
+            padding-top: 100px;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            overflow: auto;
+            background-color: rgb(204, 198, 198);
+			background-color:  rgba(204, 198, 198, 0.3);
+        }
+
+        .close {
+            color: #aaaaaa;
+            float: right;
+            font-size: 28px;
+            font-weight: bold;
+        }
+        .close:hover,
+        .close:focus {
+            color: #000;
+            text-decoration: none;
+            cursor: pointer;
+        }
+
+		.top-nav {
+			margin: 5px;
+			margin-left: 10px;
+			margin-right: 10px;
+			font-size: 20px;
+			float: right;
+			
+		}
+		li{
+			display: inline-block;
+		}
+		li a:hover{
+			padding: 15px;
+			background-color: #5555ff;
+		}
+
+		.nav-bar{
+			width: 18%;
+			height: 95%;
+			margin: 20px 0px 20px 1%;
+			background-color:  rgb(204, 198, 198);
+			float: left;
+		}
+
+		.wrap{
+			display: inline;
+		}
+
+		.content{
+			box-shadow: 2px 10px 5px black;
+			margin: 20px 1% 20px 0px;
+			height: 95%;
+			width: 80%;
+			background-color:  rgb(154, 150, 150);
+			float: left;
+		}
+
+		#nav-header{
+			background-color:  rgb(124, 120, 120);
+			height: 50px;
+		}
+		.nav-content{
+			background-color:  rgb(184, 180, 180);
+			height: 50px;
+		}
+		</style>
 	</head>
 	<body>
 		<!-- CONTENT -->
+		<div id="image-box">
+			<img src="../../img/banner2.jpg" class="header">
+		</div>
+
+		<div id="signup">
+			<p>You are logged in as </p>
+			<div class="top-nav">
+				<li><a href=""><i class="fa fa-sign-out"></i></a></li>
+			</div>
+			<div class="top-nav">
+				<li><a href=""><i class="fa fa-user"></i></a></li>
+			</div>
+			<div class="top-nav">
+				<li><a href=""><i class="fa fa-envelope"></i></a></li>
+			</div>
+			<div class="top-nav">
+				<li><a href=""><i class="fa fa-newspaper-o"></i></a></li>
+			</div>
+			<div class="top-nav">
+				<li><a href=""><i class="fa fa-home"></i></a></li>
+			</div>
+		</div>
+		<div class="wrap">
+			<div class="nav-bar">
+				<div id="nav-header">a
+				</div>
+				<div class="nav-content">a
+				</div>
+			</div>
+			<div class="content">
+			
+			</div>
+		</div>
+		<?php
+			if(isset($_POST['login'])){
+				header("Location:login.php");
+				$should_show_modal = 0;
+			}
+		?>
 	</body>
 </html>
